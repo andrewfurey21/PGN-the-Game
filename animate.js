3114a7ac035eaa09158deece6a4b3d38a2e84007
HYP3RION <39529724+andrewfurey21@users.noreply.github.com>
2019-05-14 20:22:31 +0100

    Update animate.js

--- animate.js
+++ animate.js
@@ -16,11 +16,87 @@ function animate(){
   player.move();
 }
 
-window.onkeydown = e => {
-     e.preventDefault();
 
-     if(e.keyCode == 37 || e.keyCode == 65) player.callDirection(-1, 0);
-     if(e.keyCode == 38 || e.keyCode == 87) player.callDirection(0, -1);
-     if(e.keyCode == 39 || e.keyCode == 68) player.callDirection(1, 0);
-     if(e.keyCode == 40 || e.keyCode == 83) player.callDirection(0, 1);
- };
+onKeyPress(dir => {
+  if (dir == 'up'){
+    player.callDirection(0, -1);
+  }
+
+  if (dir == 'down'){
+    player.callDirection(0, 1);
+  }
+
+  if (dir == 'left'){
+    player.callDirection(-1, 0);
+  }
+
+  if (dir == 'right'){
+    player.callDirection(1, 0);
+  }
+});
+
+
+
+function onKeyPress(callback) {
+  let keys = {},
+      keysCount = 0,
+      interval = null,
+      trackedKeys = {
+      87: true, // w
+      83: true, // s
+      65: true, // a
+      68: true, // d
+      37: true, // left arrow
+      38: true, // up arrow
+      39: true, // right arrow
+      40: true // down arrow
+  };
+
+  window.onkeydown = e => {
+    let code = e.which;
+
+    if (trackedKeys[code]) {
+      if (!keys[code]) {
+        keys[code] = true;
+        keysCount++;
+      }
+
+
+
+      if (interval === null) {
+        interval = setInterval( () => {
+          let direction = '';
+
+          if (keys[87] || keys[38]) {
+            direction = 'up';
+          } else if (keys[83] || keys[40]) {
+            direction = 'down';
+          }
+
+
+          if (keys[65] || keys[37]) {
+            direction += 'left';
+          } else if (keys[68] || keys[39]) {
+            direction += 'right';
+          }
+          callback(direction);
+        }, 1000 / 50);
+      }
+    }
+  }
+
+  window.onkeyup = e => {
+    let code = e.which;
+
+    if (keys[code]) {
+      delete keys[code];
+      keysCount--;
+    }
+
+    if ((trackedKeys[code]) && (keysCount === 0)) {
+      clearInterval(interval);
+      interval = null;
+      callback('none');
+    }
+  }
+}
